7388333f329eb29183a7a01dc1f2bb711528247b
Shyam <shyamenk@gmail.com>
2023-04-05 13:22:30 +0530

    Loader added..

--- components/products/ProductDetailPage.tsx
+++ components/products/ProductDetailPage.tsx
@@ -23,85 +23,89 @@ const ProductDetailPage = ({ product }: Props) => {
     router.isReady && setIsLoading(false);
   }, [router.isReady]);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   return (
-    <div>
-      <div className="min-h-screen mt-6 bg-primary">
-        <div className="px-4 py-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="flex flex-col lg:flex-row">
-            <div className="flex-none w-full lg:max-w-xl">
-              <div className="transition-all duration-300 ease-in-out border-4 border-gray-200 rounded hover:scale-110 duration-30 aspect-w-16 aspect-h-9">
-                <Image
-                  src={product.imageURL}
-                  alt="My Image"
-                  width={278}
-                  height={356}
-                  priority
-                />
-              </div>
-            </div>
-            <div className="flex-auto mt-8 lg:ml-10 lg:mt-0">
-              <h1 className="mb-2 text-3xl font-semibold text-secondary1">
-                {product.name}
-              </h1>
-              <h2>{product.description}</h2>
-              <div className="flex items-center mt-2">
-                <div className="flex">
-                  <AiFillStar className="w-5 h-5 text-red-400" />
-                  <AiFillStar className="w-5 h-5 text-red-400" />
-                  <AiFillStar className="w-5 h-5 text-red-400" />
-                  <AiFillStar className="w-5 h-5 text-red-400" />
-                  <AiFillStar className="w-5 h-5 text-red-400" />
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="min-h-screen mt-6 bg-primary">
+          <div className="px-4 py-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="flex flex-col lg:flex-row">
+              <div className="flex-none w-full lg:max-w-xl">
+                <div className="transition-all duration-300 ease-in-out border-4 border-gray-200 rounded hover:scale-110 duration-30 aspect-w-16 aspect-h-9">
+                  <Image
+                    src={product.imageURL}
+                    alt="My Image"
+                    width={278}
+                    height={356}
+                    priority
+                  />
                 </div>
-                <span className="ml-2 text-slate-500">(3 reviews)</span>
               </div>
-              <div className="mt-3">
-                <span className="text-xl text-gray-700">${product.price}</span>
-                <span className="ml-2 text-gray-500 line-through">$129.99</span>
-              </div>
+              <div className="flex-auto mt-8 lg:ml-10 lg:mt-0">
+                <h1 className="mb-2 text-3xl font-semibold text-secondary1">
+                  {product.name}
+                </h1>
+                <h2>{product.description}</h2>
+                <div className="flex items-center mt-2">
+                  <div className="flex">
+                    <AiFillStar className="w-5 h-5 text-red-400" />
+                    <AiFillStar className="w-5 h-5 text-red-400" />
+                    <AiFillStar className="w-5 h-5 text-red-400" />
+                    <AiFillStar className="w-5 h-5 text-red-400" />
+                    <AiFillStar className="w-5 h-5 text-red-400" />
+                  </div>
+                  <span className="ml-2 text-slate-500">(3 reviews)</span>
+                </div>
+                <div className="mt-3">
+                  <span className="text-xl text-gray-700">
+                    ${product.price}
+                  </span>
+                  <span className="ml-2 text-gray-500 line-through">
+                    $129.99
+                  </span>
+                </div>
 
-              <div className="flex items-center gap-4 mt-4 ">
-                <label
-                  className="block mb-2 font-bold text-secondary"
-                  htmlFor="quantity"
-                >
-                  Quantity
-                </label>
-                <input
-                  type="number"
-                  id="quantity"
-                  name="quantity"
-                  min="1"
-                  max="10"
-                  defaultValue="0"
-                  className="block h-8 p-2 bg-white border border-gray-300 rounded-md"
-                />
-              </div>
-              <div className="mt-4">
-                <button
-                  onClick={() =>
-                    addItem({
-                      id: product.id,
-                      name: product.name,
-                      quantity: product.quantity,
-                      price: product.price,
-                      image: product.imageURL,
-                      description: product.description
-                    })
-                  }
-                  className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600"
-                >
-                  Add to cart
-                </button>
+                <div className="flex items-center gap-4 mt-4 ">
+                  <label
+                    className="block mb-2 font-bold text-secondary"
+                    htmlFor="quantity"
+                  >
+                    Quantity
+                  </label>
+                  <input
+                    type="number"
+                    id="quantity"
+                    name="quantity"
+                    min="1"
+                    max="10"
+                    defaultValue="0"
+                    className="block h-8 p-2 bg-white border border-gray-300 rounded-md"
+                  />
+                </div>
+                <div className="mt-4">
+                  <button
+                    onClick={() =>
+                      addItem({
+                        id: product.id,
+                        name: product.name,
+                        quantity: product.quantity,
+                        price: product.price,
+                        image: product.imageURL,
+                        description: product.description
+                      })
+                    }
+                    className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600"
+                  >
+                    Add to cart
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- components/products/ProductsByCategory.tsx
+++ components/products/ProductsByCategory.tsx
@@ -21,51 +21,52 @@ const ProductsByCategory = ({ products }: Props) => {
     router.isReady && setIsLoading(false);
   }, [router.isReady]);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
   return (
     <>
-      <section className="bg-primary">
-        <div className="container max-w-6xl px-6 py-10 mx-auto">
-          <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl">
-            {slug}
-          </h1>
-          <div className="grid grid-cols-1 gap-10 mt-8 lg:grid-cols-4 sm:grid-cols-3">
-            {products.map((item) => (
-              <Link
-                key={item.id}
-                href={{
-                  pathname: `/categories/${slug}/${item.name}`,
-                  query: { productId: item.id }
-                }}
-              >
-                <div className="transition-all duration-300 ease-in-out border-2 border-t-0 rounded-md bg-slate-50 hover:scale-125 ">
-                  <div className="">
-                    <Image
-                      src={item.imageURL}
-                      alt="My Image"
-                      width={278}
-                      height={356}
-                    />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <section className="bg-primary">
+          <div className="container max-w-6xl px-6 py-10 mx-auto">
+            <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl">
+              {slug}
+            </h1>
+            <div className="grid grid-cols-1 gap-10 mt-8 lg:grid-cols-4 sm:grid-cols-3">
+              {products.map((item) => (
+                <Link
+                  key={item.id}
+                  href={{
+                    pathname: `/categories/${slug}/${item.name}`,
+                    query: { productId: item.id }
+                  }}
+                >
+                  <div className="transition-all duration-300 ease-in-out border-2 border-t-0 rounded-md bg-slate-50 hover:scale-125 ">
+                    <div className="">
+                      <Image
+                        src={item.imageURL}
+                        alt="My Image"
+                        width={278}
+                        height={356}
+                      />
+                    </div>
+                    <div className="p-2 mt-2 mb-2 text-center ">
+                      <span className="block text-xs font-medium tracking-widest uppercase">
+                        {item.name}
+                      </span>
+                      <h2 className="tracking-wide max-h-12">
+                        {truncate(item.description)}
+                      </h2>
+                      <h2 className="font-semibold tracking-wide ">
+                        Price &#x20B9;-{item.price}
+                      </h2>
+                    </div>
                   </div>
-                  <div className="p-2 mt-2 mb-2 text-center ">
-                    <span className="block text-xs font-medium tracking-widest uppercase">
-                      {item.name}
-                    </span>
-                    <h2 className="tracking-wide max-h-12">
-                      {truncate(item.description)}
-                    </h2>
-                    <h2 className="font-semibold tracking-wide ">
-                      Price &#x20B9;-{item.price}
-                    </h2>
-                  </div>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 };
