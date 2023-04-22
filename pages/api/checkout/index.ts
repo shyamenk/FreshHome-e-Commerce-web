a2c42a14303f397acd0490129bab81c13422bd4c
Shyam <shyamenk@gmail.com>
2023-04-22 20:33:17 +0530

    env Changed

--- pages/api/checkout/index.ts
+++ pages/api/checkout/index.ts
@@ -2,7 +2,7 @@ import { CartItem } from '@/context/cartContext';
 import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
   apiVersion: '2022-11-15'
 });
 
